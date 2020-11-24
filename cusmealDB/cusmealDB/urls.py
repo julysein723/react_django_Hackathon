b4d3ca4ecaf7d098a4307594c4f2d5cdf852e733
tpxn625 <58384042+tpxn625@users.noreply.github.com>
2020-11-25 04:49:48 +0900

    .

--- cusmealDB/cusmealDB/urls.py
+++ cusmealDB/cusmealDB/urls.py
@@ -1,5 +1,7 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +12,4 @@ urlpatterns = [
     path('post/', include('post.urls')),
     path('reservation/', include('reservation.urls')),
     path('utensil/', include('utensil.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cusmealDB/post/models.py
+++ cusmealDB/post/models.py
@@ -1,11 +1,21 @@
 from django.db import models
+from uuid import uuid4
+import os
+
+def uuid_name(instance, filename):
+    uuid = uuid4().hex
+    extension = os.path.splitext(filename)[-1].lower()
+    return '/'.join([
+        'post',
+        uuid+extension,
+    ])
 
 class Post(models.Model):
     nickname = models.CharField('닉네임',max_length=20)
     date = models.CharField('날짜',max_length=20)
     time = models.CharField('시간',max_length=20)
     context = models.CharField('글내용',max_length=100)
-    photo = models.ImageField('사진')
+    photo = models.ImageField('사진',upload_to=uuid_name)
 
     def __str__(self):
         return self.nickname
