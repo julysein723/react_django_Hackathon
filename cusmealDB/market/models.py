b6ea763bdab74aa6be5651a4fc4eceb192be6296
tpxn625 <58384042+tpxn625@users.noreply.github.com>
2020-11-25 02:07:09 +0900

    밀키트 재료 데이터 추가, 시장 주소 구조 변경 (address -> 두 개로 분리 UI위해)

--- cusmealDB/market/models.py
+++ cusmealDB/market/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Market(models.Model):
     name = models.CharField('시장명',max_length=20)
     photo = models.ImageField('사진')
-    address = models.CharField('주소',max_length=50)
-
+    address = models.CharField('주소1',max_length=50)
+    address2 = models.CharField('주소2',max_length=50, default = "")
     def __str__(self):
         return self.name
